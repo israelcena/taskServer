beafba52dbcd5440ba7f6f566e811c88c00fb132
Israel Cena <israelsjm@gmail.com>
2022-04-19 00:48:29 -0300

    create Api Rest Routes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,6 +2,27 @@ const getAllTasks = (req, res, next) => {
 	res.send("All Tasks!");
 };
 
+const getOneTask = (req, res, next) => {
+	res.send("One Task!");
+};
+
+const includeTask = (req, res, next) => {
+	res.send("Include Task!");
+};
+
+const deleteTask = (req, res, next) => {
+	res.send("Delete Task!");
+};
+
+const updateTask = (req, res, next) => {
+	res.send("Update Task!");
+};
+
+
 module.exports = {
 	getAllTasks,
+	getOneTask,
+	includeTask,
+	deleteTask,
+	updateTask
 };
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllTasks } = require("../controllers/tasks");
+const {
+	getAllTasks,
+	getOneTask,
+	includeTask,
+	updateTask,
+	deleteTask,
+} = require("../controllers/tasks");
 
-router.route("/").get(getAllTasks);
+router.route("/").get(getAllTasks).post(includeTask);
+router.route("/:id").get(getOneTask).patch(updateTask).delete(deleteTask);
 
 module.exports = router;
