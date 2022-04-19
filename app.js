5ed3e1dccbeda3c8bb163013957d2fabaacd4b38
Israel Cena <israelsjm@gmail.com>
2022-04-19 00:26:15 -0300

    create tasks routers

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const indexRouter = require('./routes/index');
+const tasksRouter = require('./routes/tasks');
 
 const app = express();
 
@@ -13,7 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Use the routes
 app.use('/', indexRouter);
+app.use('/tasks', tasksRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
