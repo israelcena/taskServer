9f11ea23a5a295cdd3f49b392080ee9415b2ffb2
Israel Cena <israelsjm@gmail.com>
2022-04-19 09:26:33 -0300

    include create task and findAll tasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,17 @@
-const getAllTasks = (req, res, next) => {
-	res.send("All Tasks!");
+const Task = require("../models/Task");
+
+const getAllTasks = async (req, res, next) => {
+	const allTasks = await Task.find({});
+	res.status(200).json({ allTasks });
 };
 
 const getOneTask = (req, res, next) => {
-	res.json({id:req.params.id});
+	res.json({ id: req.params.id });
 };
 
-const includeTask = (req, res, next) => {
-	res.json(req.body);
+const includeTask = async (req, res, next) => {
+	const task = await Task.create(req.body);
+	res.status(201).json({ task });
 };
 
 const updateTask = (req, res, next) => {
