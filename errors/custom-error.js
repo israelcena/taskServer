35604cf832e88676209d0556f76bf797f530a643
Israel Cena <israelsjm@gmail.com>
2022-04-20 07:14:53 -0300

    createCustomError

--- errors/custom-error.js
+++ errors/custom-error.js
@@ -1,6 +1,12 @@
-class CustomApiError extends Error{
-  constructor(message, statusCode) {
-    super(message);
-    this.statusCode = statusCode;
-  }
+class CustomApiError extends Error {
+	constructor(message, statusCode) {
+		super(message);
+		this.statusCode = statusCode;
+	}
 }
+
+const createCustomError = (message, statusCode) => {
+	return new CustomApiError(message, statusCode);
+};
+
+module.exports = { createCustomError, CustomApiError };
