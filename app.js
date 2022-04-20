620f5dd6baa39446ac7f4523fd10d44c64c62ea3
Israel Cena <israelsjm@gmail.com>
2022-04-20 06:58:12 -0300

    organize and comment code

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
+// imports
 const createError = require("http-errors");
 const express = require("express");
-// const path = require("path");
+// const path = require("path"); // not used
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const healthCheck = require("./routes/health-check");
 const tasksRouter = require("./routes/tasks");
-require("dotenv").config();
 const connectDB = require("./db/connect");
 const notFound = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+
+require("dotenv").config();
 const app = express();
 
+// express middleware
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
