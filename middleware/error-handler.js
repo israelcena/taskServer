29b444b2936a123c3fc544fceb8c7d42d6800e58
Israel Cena <israelsjm@gmail.com>
2022-04-20 08:35:17 -0300

    Creating default errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,16 @@
+const { CustomApiError } = require("../errors/custom-error");
 const errorHandlerMiddleware = (err, req, res, next) => {
 	console.error(err);
-	return res.status(err.statusCode).json({
+
+	if (err instanceof CustomApiError)
+		return res.status(err.statusCode).json({
+			message: err.message,
+		});
+
+	if (err.name === "ValidationError")
+		return res.status(403).json({ message: err.message });
+
+	return res.status(500).json({
 		message: err.message,
 	});
 };
