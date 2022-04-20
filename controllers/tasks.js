9d6cbc9fa29329ebd53aae66b005cc373e7d1225
Israel Cena <israelsjm@gmail.com>
2022-04-20 08:01:42 -0300

    include asyncWrapper and Custom Error

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,47 +1,48 @@
 const Task = require("../models/Task");
 const asyncWrapper = require("../middleware/async");
+const { createCustomError } = require("../errors/custom-error");
 
 const getAllTasks = asyncWrapper(async (req, res) => {
 	const allTasks = await Task.find({});
-	res
-		.status(200)
-		.json({
-			status: "Success",
-			data: { 
-				task: allTasks, 
-				tasksLength: allTasks.length 
-			},
-		});
+	res.status(200).json({
+		status: "Success",
+		data: {
+			task: allTasks,
+			tasksLength: allTasks.length,
+		},
+	});
 });
 
-const getOneTask = async (req, res, next) => {
+const getOneTask = asyncWrapper(async (req, res, next) => {
 	const task = await Task.findById(req.params.id);
-	console.log(task);
-	if (!task) return next.status(404).json({ msg: "Task not found" });
+	if (!task)
+		return next(
+			createCustomError(`The task with id ${req.params.id} does not exist`, 404)
+		);
 	res.status(200).json({ task });
-};
+});
 
 const includeTask = asyncWrapper(async (req, res, next) => {
 	const task = await Task.create(req.body);
 	res.status(201).json({ task });
 });
 
-const updateTask = async (req, res, next) => {
-	const { id } = req.params;
-	const task = await Task.findByIdAndUpdate(id, req.body, {
+const updateTask = asyncWrapper(async (req, res, next) => {
+	const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
 		new: true,
 		runValidators: true,
 	});
 	res.status(201).json({ task });
-};
+});
 
-const deleteTask = async (req, res) => {
-	const { id } = req.params;
-	const task = await Task.findByIdAndDelete(id);
+const deleteTask = asyncWrapper(async (req, res, next) => {
+	const task = await Task.findByIdAndDelete(req.params.id);
 	if (!task)
-		return res.status(404).json({ msg: `Task with id ${id} not found` });
-	res.status(200).json({ msg: "Task deleted" });
-};
+		return next(
+			createCustomError(`The task with id ${req.params.id} does not exist`, 404)
+		);
+	res.status(200).json({ message: "Task deleted" });
+});
 
 module.exports = {
 	getAllTasks,
